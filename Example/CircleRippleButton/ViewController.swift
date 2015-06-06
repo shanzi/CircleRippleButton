9eb22fa1468364e962ba0bf74a5be0e84ca19211
Chase Zhang <yun.er.run@gmail.com>
2015-06-06 14:50:35 +0800

    Initial commit

---
+++ Example/CircleRippleButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CircleRippleButton
+//
+//  Created by Chase Zhang on 06/06/2015.
+//  Copyright (c) 06/06/2015 Chase Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
